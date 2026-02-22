2f0e857d762a67575a2274178cf3bb3fa9d9e27d
loverboy <loverboykosu@gmail.com>
2026-02-22 10:19:14 +0900

    fix typo

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# If no version is specified as a command line argument, fetch the latest version.
+# If no version is specified, fetch the latest version from GitHub Releases.
 if [ -z "$1" ]; then
     VERSION=$(curl -s https://api.github.com/repos/loverboykosu/go-cli/releases/latest | grep -o '"tag_name": *"[^"]*"' | sed 's/"tag_name": *"//' | sed 's/"//')
     if [ -z "$VERSION" ]; then
-        echo "Failed to fetch the latest version"
+        echo "Failed to fetch the latest version."
         exit 1
     fi
 else
@@ -13,22 +13,21 @@ fi
 
 OS=$(uname -s)
 ARCH=$(uname -m)
-URL="https://github.com/<GITHUB_USER_NAME>/<GITHUB_REPO_NAME>/releases/download/${VERSION}/<GITHUB_REPO_NAME>_${OS}_${ARCH}.tar.gz"
+URL="https://github.com/loverboykosu/go-cli/releases/download/${VERSION}/my-cli-tool_${OS}_${ARCH}.tar.gz"
 
-echo "Start to install."
-echo "VERSION=$VERSION, OS=$OS, ARCH=$ARCH"
-echo "URL=$URL"
+echo "Start to install. VERSION=$VERSION, OS=$OS, ARCH=$ARCH"
+echo "Download URL=$URL"
 
 TMP_DIR=$(mktemp -d)
-curl -L $URL -o $TMP_DIR/<GITHUB_REPO_NAME>.tar.gz
-tar -xzvf $TMP_DIR/<GITHUB_REPO_NAME>.tar.gz -C $TMP_DIR
-sudo mv $TMP_DIR/<GITHUB_REPO_NAME> /usr/local/bin/<GITHUB_REPO_NAME>
-sudo chmod +x /usr/local/bin/<GITHUB_REPO_NAME>
+curl -L $URL -o $TMP_DIR/go-cli.tar.gz
+tar -xzvf $TMP_DIR/go-cli.tar.gz -C $TMP_DIR
+sudo mv $TMP_DIR/go-cli /usr/local/bin/go-cli
+sudo chmod +x /usr/local/bin/go-cli
 
 rm -rf $TMP_DIR
 
-if [ -f "/usr/local/bin/<GITHUB_REPO_NAME>" ]; then
-  echo "[SUCCESS] <GITHUB_REPO_NAME> $VERSION has been installed to /usr/local/bin"
+if [ -f "/usr/local/bin/go-cli" ]; then
+  echo "[SUCCESS] go-cli $VERSION installed to /usr/local/bin"
 else
-  echo "[FAIL] <GITHUB_REPO_NAME> $VERSION is not installed."
+  echo "[FAIL] Installation failed."
 fi
